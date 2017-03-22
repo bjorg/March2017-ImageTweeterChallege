dcde7cfa502c12cddeb093c638a1e03e8d34658d
Steve Bjorg <steve.bjorg@gmail.com>
2017-03-21 19:12:05 -0700

    added logic to post bucket and key name

--- src/ImageTweeter/LambdaHandler.cs
+++ src/ImageTweeter/LambdaHandler.cs
@@ -14,24 +14,24 @@ using Tweetinvi.Parameters;
 [assembly:LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace ImageTweeter {
     public class LambdaHandler {
-        public string Handler(ILambdaContext context) {
+        public string Handler(S3Event s3Event, ILambdaContext context) {
 
             // Level 1: Make this output when a file is uploaded to S3
             LambdaLogger.Log("Hello from The AutoMaTweeter!");
 
             // Level 2: Get the bucket name and key from event data and log to cloudwatch
-            //var bucketName = '';
-            //var keyName = '';
-
-            //LambdaLogger.Log($"The AutoMaTweeter found a file path: {bucketName}/{keyName}");
-
-            // Boss Level: Use Amazon Rekognition to get keywords about the image
+            var s3 = s3Event.Records.First().S3;
+            var bucketName = s3.Bucket.Name;
+            var keyName = s3.Object.Key;
+            LambdaLogger.Log($"bucket: {bucketName}, key: {keyName}");
 
             // Level 3: Post the image and message to twitter
             //var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
             //var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
             //var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
             //var accessTokenSecret = "fiFvNoEASqyqWo3FuFr5JKBYyWlILihVLlGCTSxfqAtlv";
+
+            // Boss Level: Use Amazon Rekognition to get keywords about the image
 
             return "Run Complete";
         }
