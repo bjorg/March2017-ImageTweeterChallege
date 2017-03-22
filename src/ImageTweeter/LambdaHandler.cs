022b68d6f76a6bacdf96cf06322d524d8ca1caef
Steve Bjorg <steve.bjorg@gmail.com>
2017-03-21 19:40:37 -0700

    tweet s3 image

--- src/ImageTweeter/LambdaHandler.cs
+++ src/ImageTweeter/LambdaHandler.cs
@@ -10,12 +10,17 @@ using RekognitionModel = Amazon.Rekognition.Model;
 using Tweetinvi;
 using TweetinviModels = Tweetinvi.Models;
 using Tweetinvi.Parameters;
+using System.Threading;
+using System.Threading.Tasks;
 
 [assembly:LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace ImageTweeter {
     public class LambdaHandler {
 
+        private IAmazonS3 s3Client;
+
         public LambdaHandler() {
+            s3Client = new AmazonS3Client();
             var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
             var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
             var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
@@ -23,7 +28,7 @@ namespace ImageTweeter {
             Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
         }
 
-        public string Handler(S3Event s3Event, ILambdaContext context) {
+        public async Task<string> Handler(S3Event s3Event, ILambdaContext context) {
 
             // Level 1: Make this output when a file is uploaded to S3
             LambdaLogger.Log("Hello from The AutoMaTweeter!");
@@ -36,7 +41,15 @@ namespace ImageTweeter {
             LambdaLogger.Log(msg);
 
             // Level 3: Post the image and message to twitter
-            Tweet.PublishTweet(msg);
+            using(var response = await s3Client.GetObjectAsync(bucketName, keyName))  {
+                var tempFile = Path.GetTempFileName();
+                await response.WriteResponseStreamToFileAsync(tempFile, false, default(CancellationToken));
+                byte[] fileBytes = File.ReadAllBytes(tempFile);
+                var media = Upload.UploadImage(fileBytes);
+                Tweet.PublishTweet("Team3!", new PublishTweetOptionalParameters {
+                    Medias = new List<TweetinviModels.IMedia> { media }
+                });
+            }
 
             // Boss Level: Use Amazon Rekognition to get keywords about the image
 
