840793e14bdb700a18fd1d9ba876805b81978e5d
Steve Bjorg <steve.bjorg@gmail.com>
2017-03-21 19:26:26 -0700

    added ability to publish

--- src/ImageTweeter/LambdaHandler.cs
+++ src/ImageTweeter/LambdaHandler.cs
@@ -14,6 +14,15 @@ using Tweetinvi.Parameters;
 [assembly:LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace ImageTweeter {
     public class LambdaHandler {
+
+        public LambdaHandler() {
+            var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
+            var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
+            var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
+            var accessTokenSecret = "fiFvNoEASqyqWo3FuFr5JKBYyWlILihVLlGCTSxfqAtlv";
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+        }
+
         public string Handler(S3Event s3Event, ILambdaContext context) {
 
             // Level 1: Make this output when a file is uploaded to S3
@@ -23,13 +32,11 @@ namespace ImageTweeter {
             var s3 = s3Event.Records.First().S3;
             var bucketName = s3.Bucket.Name;
             var keyName = s3.Object.Key;
-            LambdaLogger.Log($"bucket: {bucketName}, key: {keyName}");
+            var msg = $"bucket: {bucketName}, key: {keyName}";
+            LambdaLogger.Log(msg);
 
             // Level 3: Post the image and message to twitter
-            //var consumerKey = "GCNunS5DfXGwh8rvFAterxmXP";
-            //var consumerSecret = "fq03tBiAIAM7pB6DjRI8S69scCFiR3FibCbjz3HWfjEOPMLSQD";
-            //var accessToken = "842206967632338944-XMAH3FU86RSak57FVJmglXn4HAvNmpy";
-            //var accessTokenSecret = "fiFvNoEASqyqWo3FuFr5JKBYyWlILihVLlGCTSxfqAtlv";
+            Tweet.PublishTweet(msg);
 
             // Boss Level: Use Amazon Rekognition to get keywords about the image
 
